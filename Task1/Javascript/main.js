6f5b7cffe01f71e9dd1ef9e453b7fce20a9a706d
SHINJON MUKHERJEE <82530706+Smukh123@users.noreply.github.com>
2021-08-18 13:38:59 +0530

    Add files via upload

---
+++ Task1/Javascript/main.js
@@ -0,0 +1,10 @@
+// var video_count =1;
+// var videoPlayer = document.getElementById("episodeVideo");
+
+// function run(){
+//         video_count++;
+//         if (video_count == 4) video_count = 1;
+//         var nextVideo = ".Assets/video"+video_count+".webm";
+//         videoPlayer.src = nextVideo;
+//         videoPlayer.play();
+//    };
